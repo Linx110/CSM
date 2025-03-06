9c6e9766b59422a581a82d7eec23409376ebfcce
Linx110 <antrojeba7@gmail.com>
2025-03-06 21:58:21 +0530

    6

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask, jsonify
-Print="helo"
+
 app = Flask(__name__)
 
 @app.route('/')
 def cost_model():
-    # Mock cost data instead of AWS Cost Explorer
     response = {
         "ResultsByTime": [
             {
@@ -28,4 +28,5 @@ def cost_model():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
